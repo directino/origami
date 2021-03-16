8399a5e2bed710d1be1e3e10ba8dd37e2b4a9877
Alex Mitev <direct@abv.bg>
2021-03-16 23:21:14 +0200

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import './Navigation.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's</h1>
+
+      <nav className="Navigation">
+        <ul>
+          <li className="listItem"><img src="white-origami-bird.png" alt=""/></li>
+          <li className="listItem"><a href="#">Going to 1</a></li>
+          <li className="listItem"><a href="#">Going to 2</a></li>
+          <li className="listItem"><a href="#">Going to 3</a></li>
+          <li className="listItem"><a href="#">Going to 4</a></li>
+          <li className="listItem"><a href="#">Going to 5</a></li>
+          <li className="listItem"><a href="#">Going to 6</a></li>
+          <li className="listItem"><a href="#">Going to 7</a></li>
+          <li className="listItem"><a href="#">Going to 8</a></li>
+          <li className="listItem"><a href="#">Going to 9</a></li>
+          <li className="listItem"><a href="#">Going to 10</a></li>
+          <li className="listItem"><a href="#">Going to 11</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
