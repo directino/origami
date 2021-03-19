ff04b868bbff00827bd6d0f16f80e0b8de2b8437
Alex Mitev <direct@abv.bg>
2021-03-19 15:00:57 +0200

    Extract components from header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import './App.css';
 import './Navigation.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's</h1>
-
-      <nav className="Navigation">
-        <ul>
-          <li className="listItem"><img src="white-origami-bird.png" alt=""/></li>
-          <li className="listItem"><a href="#">Going to 1</a></li>
-          <li className="listItem"><a href="#">Going to 2</a></li>
-          <li className="listItem"><a href="#">Going to 3</a></li>
-          <li className="listItem"><a href="#">Going to 4</a></li>
-          <li className="listItem"><a href="#">Going to 5</a></li>
-          <li className="listItem"><a href="#">Going to 6</a></li>
-          <li className="listItem"><a href="#">Going to 7</a></li>
-          <li className="listItem"><a href="#">Going to 8</a></li>
-          <li className="listItem"><a href="#">Going to 9</a></li>
-          <li className="listItem"><a href="#">Going to 10</a></li>
-          <li className="listItem"><a href="#">Going to 11</a></li>
-        </ul>
-      </nav>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+    return (
+        <nav className="Navigation">
+        <ul>
+          <li className="listItem"><img src="white-origami-bird.png" alt=""/></li>
+          <li className="listItem"><a href="#">Going to 1</a></li>
+          <li className="listItem"><a href="#">Going to 2</a></li>
+          <li className="listItem"><a href="#">Going to 3</a></li>
+          <li className="listItem"><a href="#">Going to 4</a></li>
+          <li className="listItem"><a href="#">Going to 5</a></li>
+          <li className="listItem"><a href="#">Going to 6</a></li>
+          <li className="listItem"><a href="#">Going to 7</a></li>
+          <li className="listItem"><a href="#">Going to 8</a></li>
+          <li className="listItem"><a href="#">Going to 9</a></li>
+          <li className="listItem"><a href="#">Going to 10</a></li>
+          <li className="listItem"><a href="#">Going to 11</a></li>
+        </ul>
+      </nav>
+    );
+};
+
+export default Header;
